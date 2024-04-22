51fbb21cabe2c192ad55efe8226349c4e396508a
Hue <154227120+AKArien0@users.noreply.github.com>
2024-04-22 23:29:24 +0200

    Corrected

--- Input.hpp
+++ Input.hpp
@@ -2,21 +2,22 @@
 
 namespace Input{
 
-	static void interrupt_change_handler(void *p);
-
 	class Button{
 		private:
 			volatile int pin;
 			volatile bool current_state, last_state;
+			volatile unsigned int debounce_time, debounce_last_measure;
 
 		public:
 			Button(int set_pin);
 			void interrupt_change();
-			void begin();
+			void begin(mode, debounce);
 			bool is_pressed();
 			bool is_just_pressed();
 			bool is_just_released();
 			~Button();
 	};
+
+	static void interrupt_change_handler(void *p);
 
 }
